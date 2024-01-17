2b2586dbc59aa7591d0aeff1a70bf1372a2b0d16
AlexVj12 <85960309+AlexVj12@users.noreply.github.com>
2024-01-16 19:29:43 -0500

    Update routing.py

--- routing.py
+++ routing.py
@@ -1,17 +1,11 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from django.core.asgi import get_asgi_application
 from  AppRehabilitation.views  import RutinaLuxacionConsumer, RutinaLesionMediaConsumer
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 
 application = ProtocolTypeRouter({
-    # Django's ASGI application to handle traditional HTTP requests
-    "http": get_asgi_application,
-    "https": get_asgi_application,
-
-    # WebSocket chat handler
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
